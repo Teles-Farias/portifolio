4043aad4706ebbf3db3015955a3073469ffa4d39
Teles <telesbrunosf@gmail.com>
2023-03-08 17:15:32 -0300

    atualizando

---
+++ js/cod.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Tech Devoloper","Wood Industrial Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
